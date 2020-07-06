773df7674de212c82393a620e628367820243b03
neodonator <k.baral229@gmail.com>
2020-07-06 15:00:28 -0400

    remaming to posts

--- ClientApp/src/components/navMenu.js
+++ ClientApp/src/components/navMenu.js
@@ -53,7 +53,7 @@ export class NavMenu extends Component {
         >
           <Container>
             <NavbarBrand tag={Link} to="/">
-              Blogs
+              Posts
             </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse
